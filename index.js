5688062b529fd83f1fcddfa44c777c93b3b5f56e
Harish168 <harishleela05@gmail.com>
2021-07-22 13:44:03 +0530

    done front end

---
+++ index.js
@@ -0,0 +1,9 @@
+const saveChanges = () => {
+    const taskDate ={
+        id: `${Date.now()}`,   //unique number for id
+        imageUrl: "",
+        taskTitle: "",
+        taskType: "",
+        taskDescription: "",
+    };
+};
